7b08e4a83050123d528f5cb892e241abc3b24e03
Muhammed Kağan ÜLKER <m.kaganulker@gmail.com>
2024-10-08 00:07:20 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   ConsoleApp1/Program.cs
    	modified:   ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.dll
    	modified:   ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.exe
    	modified:   ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.pdb
    	modified:   ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
    	modified:   ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfoInputs.cache
    	modified:   ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.csproj.FileListAbsolute.txt
    	modified:   ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.dll
    	modified:   ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.pdb
    	new file:   ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.sourcelink.json
    	modified:   ConsoleApp1/obj/Debug/net8.0/apphost.exe
    	modified:   ConsoleApp1/obj/Debug/net8.0/ref/ConsoleApp1.dll
    	modified:   ConsoleApp1/obj/Debug/net8.0/refint/ConsoleApp1.dll
    	modified:   MauiApp1/obj/Debug/net8.0-android/MauiApp1.AssemblyInfo.cs
    	modified:   MauiApp1/obj/Debug/net8.0-android/MauiApp1.AssemblyInfoInputs.cache
    	modified:   MauiApp1/obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
    	modified:   MauiApp1/obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfoInputs.cache
    	modified:   MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
    	modified:   MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfoInputs.cache
    	modified:   MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
    	modified:   MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfoInputs.cache

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hell!");
 
+int age = 25;
+double salary = 50000;
+double infilation_each_year = .5;
+string name = "Johnny";
+int year = 2024;
+bool isEmployee = true;
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Salary: " + salary);
+Console.WriteLine("Is Employee: " + isEmployee);
+double actual_salary;
+actual_salary = infilation_each_year*salary;
+while (age<=60){
+Console.WriteLine("Salary: " + salary + " on Year: " + year);
+
+Console.WriteLine("after infilation its worth is: "+actual_salary);
+salary += 5000;
+year++;
+age++;
+actual_salary *= infilation_each_year;
+}
+Console.WriteLine(name+ " Retired at the Year: "+year+" and with the salary of "+salary);
+Console.WriteLine("after infilation its worth is: "+actual_salary);
+Console.WriteLine(name+" is now "+50000/actual_salary+" times poorer when he started working");
--- MauiApp1/obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0e4471da7c8cb242b2739717090e3ba1256e0b6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
